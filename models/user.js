af9978fbeb16eae471b88e7d0bdb95db47833613
nagiarjun05 <nagiarjun05@gmail.com>
2023-02-02 19:37:12 +0530

    Delete function added in cart

--- models/user.js
+++ models/user.js
@@ -32,6 +32,34 @@ class User{
     .updateOne({_id:new Object(this._id)},
     {$set:{cart:updatedCart}})
   }
+  getCart(){
+    const db=getDb()
+    const productIds=this.cart.items.map(i=>{
+      return i.productId
+    })
+    return db.collection('products')
+    .find({_id: {$in:productIds}})
+    .toArray()
+    .then(products=>{
+      return products.map(p=>{
+        return {...p,
+          quantity: this.cart.items.find(i=>{
+            return p._id.toString()==i.productId.toString()
+          }).quantity
+        }
+      })
+    })
+  }
+  deleteItemFromCart(productId){
+    const updatedCartItems=this.cart.items.filter(item=>{
+      return item.productId.toString()!==productId.toString()
+    })
+    const db=getDb();
+    return db.collection('users')
+    .updateOne({_id:new Object(this._id)},
+    {$set:{cart:{items:updatedCartItems}}})
+  }
+
   static findById(userId){
     const db=getDb()
     return db.collection('users')
