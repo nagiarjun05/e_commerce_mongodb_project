82c9f49cb9df3839ecaa3b4017a0040926ea8ef2
nagiarjun05 <nagiarjun05@gmail.com>
2023-02-02 20:15:41 +0530

    Order functionality added

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongodb=require('mongodb');
+const { get } = require('../routes/admin');
 const getDb=require('../util/database').getDb
 
 class User{
@@ -58,6 +59,33 @@ class User{
     return db.collection('users')
     .updateOne({_id:new Object(this._id)},
     {$set:{cart:{items:updatedCartItems}}})
+  }
+
+  placeOrder(){
+    const db=getDb()
+    return this.getCart()
+    .then((products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new mongodb.ObjectId(this._id),
+          name:this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+      .then(result=>{
+        this.cart={items:[]};
+        return db.collection('users')
+        .updateOne({_id:new Object(this._id)},
+        {$set:{cart:{items:[]}}})
+    })
+  }))
+}
+
+  getOrders(){
+    const db=getDb()
+    return db.collection('orders').find({'user._id':new mongodb.ObjectId(this._id)})
+    .toArray()
   }
 
   static findById(userId){
